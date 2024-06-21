595eaf2ae80b17b6d9ae83e5924f79870e05a909
WANG <wangqianlf@live.com>
2024-06-21 10:56:35 +0200

    création de Servlet TestThreadSafeServlet et son code afin d'essayer de lancer une même requête 2 fois en parallèle

---
+++ src/main/java/com/travauxpratiques/premiereapp/controller/TestThreadSafeServlet.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.travauxpratiques.premiereapp.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author wangq
+ */
+@WebServlet(name = "TestThreadSafeServlet", urlPatterns = {"/testThread"})
+public class TestThreadSafeServlet extends HttpServlet {
+
+    String montant;
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Ici on détermine le montant que l'utilisateur souhaite retirer en récupérant un paramètre nommé "montant"
+        montant = request.getParameter("montant");
+        //Ce code fait une pause à l'exécution pendant 10 secondes. 
+        try {
+            //Cela signifie que l'on souhaite que le processus d'exécution courant fasse une pause de 10000 millisecondes = 10 secondes
+            //On a créé cette pause pour essayer de lancer cette requête 2 fois en parallèle à destination de la même Servlet
+            Thread.sleep(10000);
+        } catch (InterruptedException ex) {
+            //Ici on a un "try catch" parce que cette pause peut lever des execeptions, mais ici on n'entre pas dans le détail. 
+            //Il faut importer la CLass "java.util.logging.Level"
+            Logger.getLogger(TestThreadSafeServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        PrintWriter out = response.getWriter();
+        //A la fin de la méthode "doGet" on affiche le String "montant" souhaité. 
+        out.print("<html><body>Vous avez retiré " + montant + " euros</body></html>");
+    }
+
+}
