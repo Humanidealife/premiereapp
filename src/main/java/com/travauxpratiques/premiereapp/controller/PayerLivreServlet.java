ff7495f50e8b828ab7606d76f6ac458861e1fadc
WANG <wangqianlf@live.com>
2024-06-24 12:02:21 +0200

    modif commentaire dans SelectionLivreServlet, ajout du numéro de Session et son affichage dans PayerLivreServlet

--- src/main/java/com/travauxpratiques/premiereapp/controller/PayerLivreServlet.java
+++ src/main/java/com/travauxpratiques/premiereapp/controller/PayerLivreServlet.java
@@ -23,11 +23,17 @@ public class PayerLivreServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        //Si l'on fait anouveau un "request.getSession()", on ne va pas regénérer un nouveau numéro de Session. 
+        //  On va juste retrouver notre numéro de Session d'origine.
+        //On peut essayer de vérifier en récupérant à nouveau ce numéro de Session
+        String numeroSessionPLS = request.getSession().getId();
         //On récupère le numéro de carte saisi dans le formulaire
         String numeroCarte = request.getParameter("numeroCarte");
         PrintWriter out = response.getWriter();
         //On affiche tout simplement le message suivant
         out.print("<html><body>Paiement effectué avec la carte du numéro " + numeroCarte + "<br>");
+        //On affiche ce numéro récuéré ici
+        out.print("Le numéro de session est: " + numeroSessionPLS + "<br>");
         out.print("</body><html>");
     }
 }
