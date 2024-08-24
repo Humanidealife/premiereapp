03156b6e8a63c5a30fe2f76728187c7c863b0e97
WANG <wangqianlf@live.com>
2024-08-24 12:02:39 +0200

    modif dans application.js, saisie-nombres.html et affichage-somme.jsp avant d'utiliser Ajax

--- src/main/webapp/WEB-INF/affichage-somme.jsp
+++ src/main/webapp/WEB-INF/affichage-somme.jsp
@@ -5,17 +5,5 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Somme de nombres</title>
-    </head>
-    <body>
-        <!-- La variable "somme" n'existe pas dans cette page JSP, mais cela est une information dont on dispose dans le Scope
-                "request". Il nous suffit d'utiliser les JSP EL. 
-            Il est aussi possible de laisser notre JSP rechercher dans tous les Scope où se trouve cet attribut.
-        -->
-        La somme des deux nombres fournis est ${requestScope.somme} 
-    </body>
-</html>
+
+<p>La somme des deux nombres fournis est ${requestScope.somme}</p>
--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -3,4 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
 
-
+//On va écrire le code ci-dessous qui signifie : jQuery trouve-moi dans le DOM toutes les balises "form"
+//  (dans notre cas, il n'y en a qu'une seule).
+//Après cette balise "form", ajoute-moi un nouveau noeud qui s'appelle "x"
+//"jQuery" est une fonction particulière de JavaScript qui est fournie par la librairie "jQuery" 
+//  (c'est équivalent d'une méthode en Java). En général, on remplace cette fonction par son alias qui est $.
+//Il va falloir retrouver le résultat "x" à partir du Serveur. Il faut interroger le Serveur pour obtenir le "x",
+//  c'est pour cela, on va utiliser "Ajax".
+//Ajax n'est pas une technologie, c'est simplement une technique qui va donner lieu à une manière particulière 
+//  d'interagir avec le Serveur. 
+//En gros, on va demander au navigateur de contacter une URL sans se préoccuper de ce qui est retourné (plus concrètement
+//  ce qui sera retourné par le navigateur ne doit pas remplacer le contenu actuel, il s'agira seulement avec Ajax de mettre
+//  à disposition deux JavaScript ce qui est retourné, libre à nous d'en faire ce que l'on veut).
+$("form").after(x);
